8d84ea0054e16b6a386b695b5e8e072056a2bb74
Nilsson03 <nilsson_nicholas@mail.ru>
2025-08-25 19:10:47 +0300

    Removed EntityAnimator

--- src/main/java/ru/nilsson03/library/bukkit/animation/builder/AnimationBuilder.java
+++ src/main/java/ru/nilsson03/library/bukkit/animation/builder/AnimationBuilder.java
@@ -3,13 +3,10 @@ package ru.nilsson03.library.bukkit.animation.builder;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import ru.nilsson03.library.NPlugin;
-import ru.nilsson03.library.bukkit.animation.entity.EntityAnimator;
 import ru.nilsson03.library.bukkit.animation.particle.ParticleAnimator;
 
 import java.util.ArrayList;
@@ -23,15 +20,10 @@ public class AnimationBuilder {
     private Consumer<Location> completionHandler;
     private Location startLocation;
 
-    // Для ParticleAnimator
     private Particle particle;
     private double size;
     private Player targetPlayer;
-
-    // Для EntityAnimator
-    private ItemStack item;
-    private Entity animatedEntity;
-    private NPlugin plugin;
+    private final NPlugin plugin;
 
     public AnimationBuilder(NPlugin plugin) {
         this.plugin = plugin;
@@ -41,26 +33,17 @@ public class AnimationBuilder {
         return new AnimationBuilder(plugin);
     }
 
-    // Настройка частиц
     public AnimationBuilder withParticle(Particle particle, double size) {
         this.particle = particle;
         this.size = size;
         return this;
     }
 
-    // Настройка предмета
-    public AnimationBuilder withItem(ItemStack item) {
-        this.item = item;
-        return this;
-    }
-
-    // Настройка игрока
     public AnimationBuilder forPlayer(Player player) {
         this.targetPlayer = player;
         return this;
     }
 
-    // Добавление шагов анимации
     public AnimationBuilder addStep(Consumer<AnimationStep> stepConfigurator) {
         AnimationStep step = new AnimationStep();
         stepConfigurator.accept(step);
@@ -68,7 +51,6 @@ public class AnimationBuilder {
         return this;
     }
 
-    // Управление потоком
     public AnimationBuilder loop(boolean loop) {
         this.loop = loop;
         return this;
@@ -84,19 +66,15 @@ public class AnimationBuilder {
         return this;
     }
 
-    // Запуск анимации
     public UniversalAnimation buildAt(Location location) {
         this.startLocation = location;
 
         if (particle != null) {
             return new ParticleAnimationImpl();
-        } else if (item != null) {
-            return new EntityAnimationImpl();
         }
         throw new IllegalStateException("Не указан тип анимации (частицы или предмет)");
     }
 
-    // Реализация для частиц
     private class ParticleAnimationImpl implements UniversalAnimation {
         private BukkitTask task;
         private int currentStep = 0;
@@ -149,67 +127,6 @@ public class AnimationBuilder {
         }
     }
 
-    // Реализация для предметов
-    private class EntityAnimationImpl implements UniversalAnimation {
-        private BukkitTask task;
-        private int currentStep = 0;
-
-        @Override
-        public void start() {
-            animatedEntity = EntityAnimator.createItemEntity(startLocation, item);
-
-            task = new BukkitRunnable() {
-                @Override
-                public void run() {
-                    if (currentStep >= steps.size()) {
-                        if (loop) {
-                            currentStep = 0;
-                        } else {
-                            cancel();
-                            if (completionHandler != null) {
-                                completionHandler.accept(startLocation);
-                            }
-                            animatedEntity.remove();
-                            return;
-                        }
-                    }
-
-                    AnimationStep step = steps.get(currentStep);
-                    executeEntityStep(step);
-                    currentStep++;
-                }
-            }.runTaskTimer(Bukkit.getPluginManager().getPlugins()[0], 0, delayBetweenSteps);
-        }
-
-        private void executeEntityStep(AnimationStep step) {
-            if (step.rotation != null) {
-                EntityAnimator.animateRotation(animatedEntity,
-                        step.rotation.yaw, step.rotation.pitch, step.rotation.roll, step.duration);
-            }
-
-            if (step.movement != null) {
-                EntityAnimator.animateMovement(animatedEntity, step.movement.target, step.duration);
-            }
-
-            if (step.scale != null) {
-                EntityAnimator.animateScale(animatedEntity,
-                        step.scale.x, step.scale.y, step.scale.z, step.duration);
-            }
-        }
-
-        @Override
-        public void stop() {
-            if (task != null) task.cancel();
-            if (animatedEntity != null) animatedEntity.remove();
-        }
-
-        @Override
-        public boolean isRunning() {
-            return task != null && !task.isCancelled();
-        }
-    }
-
-    // Класс для конфигурации шага
     public static class AnimationStep {
         private String shape;
         private Rotation rotation;
@@ -243,7 +160,6 @@ public class AnimationBuilder {
         }
     }
 
-    // Вспомогательные классы
     private static class Rotation {
         final float yaw, pitch, roll;
 
