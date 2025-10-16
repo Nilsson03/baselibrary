207e98b5e255288d42c72b478ee6dde9b3c31841
Nilsson03 <nilsson_nicholas@mail.ru>
2025-10-16 20:47:10 +0300

    Refactor NPlugin class: remove unused dependency check methods and related imports to streamline code and improve maintainability.

--- src/main/java/ru/nilsson03/library/NPlugin.java
+++ src/main/java/ru/nilsson03/library/NPlugin.java
@@ -4,14 +4,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 import ru.nilsson03.library.bukkit.file.BukkitDirectory;
 import ru.nilsson03.library.bukkit.file.FileRepository;
 import ru.nilsson03.library.bukkit.integration.Integration;
-import ru.nilsson03.library.bukkit.integration.PluginDependency;
 import ru.nilsson03.library.bukkit.notify.PlayerNotificationService;
 import ru.nilsson03.library.bukkit.scheduler.TaskScheduler;
 import ru.nilsson03.library.bukkit.util.log.ConsoleLogger;
 
-import java.lang.reflect.Method;
 import java.util.Optional;
-import java.util.function.Supplier;
 
 public abstract class NPlugin extends JavaPlugin {
     private BaseLibrary baseLibrary;
@@ -50,26 +47,6 @@ public abstract class NPlugin extends JavaPlugin {
             getLogger().info(getDescription().getName() + " disabled!");
         } catch (Exception e) {
             ConsoleLogger.error(baseLibrary, "Failed to disable %s plugin, reason: %s", getDescription().getName(), e.getMessage());
-        }
-    }
-
-    public static boolean checkDependency(Method method) {
-        PluginDependency annotation = method.getAnnotation(PluginDependency.class);
-        if (annotation == null) return true;
-
-        return integration.isVersionCompatible(annotation.name(), annotation.minVersion());
-    }
-
-    public static <T> T executeOrSkip(Supplier<T> action, Method method, T defaultValue) {
-        if (checkDependency(method)) {
-            return action.get();
-        }
-        return defaultValue;
-    }
-
-    public static void executeOrSkip(Runnable action, Method method) {
-        if (checkDependency(method)) {
-            action.run();
         }
     }
 
