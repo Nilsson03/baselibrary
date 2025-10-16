751aaa812189724af7af261a5d2436c25c9e9cda
Nilsson03 <nilsson_nicholas@mail.ru>
2025-10-16 20:47:56 +0300

    Refactor PluginInfo class: replace manual constructor and getter methods with Lombok annotations for cleaner code and improved maintainability.

--- src/main/java/ru/nilsson03/library/bukkit/integration/PluginInfo.java
+++ src/main/java/ru/nilsson03/library/bukkit/integration/PluginInfo.java
@@ -1,20 +1,12 @@
 package ru.nilsson03.library.bukkit.integration;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class PluginInfo {
 
     private final String pluginName;
     private final String version;
-
-    public PluginInfo(String pluginName, String version) {
-        this.pluginName = pluginName;
-        this.version = version;
-    }
-
-    public String getPluginName() {
-        return pluginName;
-    }
-
-    public String getVersion() {
-        return version;
-    }
 }
