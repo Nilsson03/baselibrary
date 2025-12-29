47f8cdfb4b33aeae74c1e1ead6af61768d62bff0
Nilsson03 <nilsson_nicholas@mail.ru>
2025-12-29 18:32:24 +0300

    Переписан на ProtocolLib

--- src/main/java/ru/nilsson03/library/bukkit/bar/UniversalActionBar.java
+++ src/main/java/ru/nilsson03/library/bukkit/bar/UniversalActionBar.java
@@ -1,16 +1,24 @@
 package ru.nilsson03.library.bukkit.bar;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.EnumWrappers;
+import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import org.bukkit.entity.Player;
-import ru.nilsson03.library.nms.NMSActionBar;
 
 public class UniversalActionBar {
-    private static final ActionBarHandler handler;
-
-    static {
-        handler = new NMSActionBar();
-    }
+    private static final ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
 
     public static void send(Player player, String message) {
-        handler.send(player, message);
+        try {
+            PacketContainer packet = protocolManager.createPacket(PacketType.Play.Server.TITLE);
+            packet.getTitleActions().write(0, EnumWrappers.TitleAction.ACTIONBAR);
+            packet.getChatComponents().write(0, WrappedChatComponent.fromText(message));
+            protocolManager.sendServerPacket(player, packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
